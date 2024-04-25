8c94355ac64c54c0efd63ea238b40061b5eb3612
Koen Luyten <kluyten@xpirit.com>
2024-04-25 14:37:27 +0200

    Add project files.

---
+++ poc-ghas-longpaths/superduperlongfoldername1/superduperlongfoldername2/superduperlongfoldername3/superduperlongfoldername4/superduperlongfoldername5/superduperlongfoldername6/superduperlongfoldername7/superduperlongfilename.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace poc_ghas_longpaths.superduperlongfoldername1.superduperlongfoldername2.superduperlongfoldername3.superduperlongfoldername4.superduperlongfoldername5.superduperlongfoldername6.superduperlongfoldername7
+{
+    internal class superduperlongfilename
+    {
+    }
+}
